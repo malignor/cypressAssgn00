b22d63717d97f45c57fcb0246fa14f648327b732
malignor <60297857+malignor@users.noreply.github.com>
2024-01-14 13:58:16 -0500

    Update cypress.config.ts

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,9 +1,8 @@
 import { defineConfig } from 'cypress'
-
 export default defineConfig({
 	projectId: 'h8wftu',
 	e2e: {
 		baseUrl: 'http://localhost:3000',
+    specPattern: "cypress/e2e/cypress-web-assessment/*.cy.ts"
 	},
-	specPattern: "**/*.cy.ts",
 })
