d128542e2a682bfc7a56e89c2fb194a9d033263e
malignor <60297857+malignor@users.noreply.github.com>
2024-01-14 15:13:15 -0500

    Delete cypress.config.js

--- cypress.config.js
+++
@@ -1,8 +0,0 @@
-const { defineConfig } = require('cypress')
-
-module.exports = defineConfig({
-	projectId: 'h8wftu',
-	e2e: {
-    specPattern: "cypress/e2e/cypress-web-assessment/*.cy.ts",
-	},
-})
