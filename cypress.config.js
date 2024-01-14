8b2a14cb839b94f28b3209118d1402aced48917c
malignor <60297857+malignor@users.noreply.github.com>
2024-01-14 16:04:25 -0500

    got it running independently

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,6 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
 	projectId: 'h8wftu',
 	e2e: {
-    specPattern: "cypress/e2e/cypress-web-assessment/*.cy.ts",
+    specPattern: "cypress/e2e/cypress-web-assessment/*.cy.*s",
 	},
 })
