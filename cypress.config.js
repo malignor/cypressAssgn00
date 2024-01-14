42897deec286485b8497971cd64e1ee2a8472b14
malignor <60297857+malignor@users.noreply.github.com>
2024-01-14 15:28:51 -0500

    Revert "Delete cypress.config.js"
    This reverts commit d128542e2a682bfc7a56e89c2fb194a9d033263e.

---
+++ cypress.config.js
@@ -0,0 +1,8 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+	projectId: 'h8wftu',
+	e2e: {
+    specPattern: "cypress/e2e/cypress-web-assessment/*.cy.ts",
+	},
+})
